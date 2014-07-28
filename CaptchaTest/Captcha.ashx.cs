810ca77967f21f6b3d7058b4124e7ddeec20b69a
i-e-b <ieb@me.com>
2014-07-28 21:22:27 +0100

    Import of old code

---
+++ CaptchaTest/Captcha.ashx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+
+namespace CaptchaTest {
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	public class Captcha : IHttpHandler {
+		protected static int WIDTH = 300;
+		protected static int HEIGHT = 120;
+
+		public void ProcessRequest (HttpContext context) {
+			List<Bitmap> frames = new List<Bitmap>();
+
+			Random rnd = new Random();
+			for (int i = 0; i < 50; i++) {
+				Color A = Color.White;
+				Color B = Color.Black;
+				frames.Add(DrawImage("159786", A, B, i));
+			}
+
+			OutputAnimatedGif(frames.ToArray(), context);
+		}
+
+		public void OutputAnimatedGif (Bitmap[] frames, HttpContext context) {
+			MemoryStream memoryStream;
+			BinaryWriter binaryWriter;
+			Byte[] buf1;
+			Byte[] buf2;
+			Byte[] buf3;
+			//Variable declaration
+
+			context.Response.ContentType = "image/gif";
+			memoryStream = new MemoryStream();
+			buf2 = new Byte[19];
+			buf3 = new Byte[8];
+			buf2[0] = 33;  //extension introducer
+			buf2[1] = 255; //application extension
+			buf2[2] = 11;  //size of block
+			buf2[3] = 78;  //N
+			buf2[4] = 69;  //E
+			buf2[5] = 84;  //T
+			buf2[6] = 83;  //S
+			buf2[7] = 67;  //C
+			buf2[8] = 65;  //A
+			buf2[9] = 80;  //P
+			buf2[10] = 69; //E
+			buf2[11] = 50; //2
+			buf2[12] = 46; //.
+			buf2[13] = 48; //0
+			buf2[14] = 3;  //Size of block
+			buf2[15] = 1;  //
+			buf2[16] = 0;  //
+			buf2[17] = 0;  //
+			buf2[18] = 0;  //Block terminator
+			buf3[0] = 33;  //Extension introducer
+			buf3[1] = 249; //Graphic control extension
+			buf3[2] = 4;   //Size of block
+			buf3[3] = 9;   //Flags: reserved, disposal method, user input, transparent color
+			buf3[4] = 1;  //Delay time low byte
+			buf3[5] = 0;   //Delay time high byte
+			buf3[6] = 255; //Transparent color index
+			buf3[7] = 0;   //Block terminator
+			binaryWriter = new BinaryWriter(context.Response.OutputStream);
+			bool first = true;
+			foreach (Bitmap image in frames) {
+				image.Save(memoryStream, ImageFormat.Gif);
+				buf1 = memoryStream.ToArray();
+
+				if (first) {
+					first = false;
+					//only write these the first time....
+					binaryWriter.Write(buf1, 0, 781); //Header & global color table
+					binaryWriter.Write(buf2, 0, 19); //Application extension
+				}
+
+				binaryWriter.Write(buf3, 0, 8); //Graphic extension
+				binaryWriter.Write(buf1, 789, buf1.Length - 790); //Image data
+
+				memoryStream.SetLength(0);
+			}
+			binaryWriter.Write(";"); //Image terminator
+			binaryWriter.Close();
+			context.Response.Flush();
+			context.Response.End();
+		}
+
+		public Bitmap DrawImage (string Code, Color A, Color B, int idx) {
+
+			Bitmap img = new Bitmap(WIDTH, HEIGHT, PixelFormat.Format24bppRgb);
+			Graphics g = Graphics.FromImage(img);
+			g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+			Rectangle r = new Rectangle(0, 0, WIDTH, HEIGHT);
+
+
+			HatchBrush background = new HatchBrush(HatchStyle.Percent50, A, B);
+			HatchBrush foreground = new HatchBrush(HatchStyle.Percent50, B, A);
+
+			Font f = new Font("Arial Black", 48);
+
+			g.FillRectangle(background, r);
+			g.DrawString(Code, f, foreground, 10, 10);
+			g.DrawString(Code, f, background,
+					8 + ((idx % 3)*2) ,
+					8 + (((idx+1) % 3)*2) 
+				);
+
+			g.Flush();
+			g.Dispose();
+
+			return img;
+		}
+
+		public bool IsReusable {
+			get {
+				return false;
+			}
+		}
+	}
+}
---
+++ GroupCaptcha/GroupCaptcha/Captcha/CaptchaImage.ashx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace GroupCaptcha.Captcha {
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	public class CaptchaImage : IHttpHandler {
+
+		private struct Pair {
+			public int group;
+			public int index;
+		}
+
+		public void ProcessRequest (HttpContext context) {
+			context.Response.ContentType = "image/gif";
+
+			int seed = int.Parse(context.Request.QueryString.ToString());
+			Random rnd = new Random(seed);
+
+			Bitmap bmp = new Bitmap(256, 256);
+
+			int[] numbers = new int[9];
+			for (int i = 0; i < numbers.Length; i++) numbers[i] = rnd.Next(1,9);
+
+			int c_gr = 15;
+			Pair[] groups = new Pair[c_gr];
+			for (int i = 0; i < groups.Length; i++) {
+				groups[i].group = i % 4;
+				groups[i].index = rnd.Next(0, 9);
+			}
+
+			using (Graphics g = Graphics.FromImage(bmp)) {
+
+				Font f = new Font("Arial Black", 24, GraphicsUnit.Point);
+					int hh = f.Height / 2;
+				g.Clear(Color.White);
+
+				// draw connections
+				foreach (Pair pr in groups) {
+					Pen pn = new Pen(cGroup(pr.group), 2.0f);
+					g.DrawLine(pn, pGroup(pr.group), pNum(pr.index, pr.group));
+				}
+
+				// draw numbers
+				for (int i = 0; i < numbers.Length; i++) {
+					Point p = pNum(i);
+					g.DrawString(numbers[i].ToString(), f, Brushes.Black, p.X, p.Y-hh);
+				}
+
+				// draw groups
+				for (int i = 0; i < 4; i++) {
+					Brush gb = new SolidBrush(cGroup(i));
+					Point p = pGroup(i);
+					g.FillEllipse(Brushes.White, new Rectangle(p.X - hh, p.Y - hh, f.Height, f.Height));
+					g.DrawString(nGroup(i).ToUpper(), f, gb, p.X-hh, p.Y - hh);
+				}
+
+				g.Flush();
+			}
+
+			bmp.Save(context.Response.OutputStream, System.Drawing.Imaging.ImageFormat.Gif);
+			context.Response.Flush();
+		}
+
+		private string nGroup (int group) {
+			switch (group) {
+				case 0: return "a";
+				case 1: return "b";
+				case 2: return "c";
+				case 3: return "d";
+				default: return "?";
+			}
+		}
+
+		private Color cGroup (int group) {
+			switch (group) {
+				case 0: return Color.FromArgb(128, 0, 0);
+				case 1: return Color.FromArgb(0, 0, 150);
+				case 2: return Color.FromArgb(0, 128, 0);
+				case 3: return Color.FromArgb(72, 0, 72);
+				default: return Color.FromArgb(0, 0, 0);
+			}
+		}
+
+		private Point pGroup (int group) {
+			switch (group) {
+				case 0: return new Point(64, 64);
+				case 1: return new Point(192, 64);
+				case 2: return new Point(64, 192);
+				case 3: return new Point(192, 192);
+				default: return new Point(0, 0);
+			}
+		}
+
+		private Point pNum (int index) {
+			return new Point((index * 28), 128);
+		}
+
+		private Point pNum (int index, int group) {
+			int off = (group < 2)?(-14):(14);
+			return new Point((index * 28) + 14, 128+off);
+		}
+
+		public bool IsReusable {
+			get {
+				return false;
+			}
+		}
+	}
+}
